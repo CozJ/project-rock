b003631cbc57cb1a9e012dd9beed4a1f3b06d282
Corey Sutcliffe <Corey.j.sutcliffe@gmail.com>
2023-02-19 14:31:49 +0000

    prisma init

--- src/providers/provider.tsx
+++ src/providers/provider.tsx
@@ -1,18 +1,21 @@
 "use client"
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { SessionProvider } from 'next-auth/react'
 import { useState } from 'react'
 
 export default function provider({
     children,
+    //session,
 }: {
     children: React.ReactNode
+    //session: any
 }) {
     const [queryClient] = useState(() => new QueryClient())
 
     return (
         <QueryClientProvider client={queryClient}>
-            {children}
+                {children}
         </QueryClientProvider>
     )
 }
