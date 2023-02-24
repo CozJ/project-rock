130dbc18a342a5055ed11d8ccacf5087e41726a8
Corey Sutcliffe <Corey.j.sutcliffe@gmail.com>
2023-02-24 14:09:50 +0000

    Bug Fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
       <head />
       <Providers>
         <body className={inter.className}>
-          <header className="w-full p-2 border-b-2 border-gray-300 shadow-xl flex flex-row justify-between">
+          <header className="w-full p-2 border-b-2 border-gray-300 shadow-xl flex flex-row justify-between items-center">
             <h1 className="text-2xl font-semibold">Project-Rock</h1>
             <AuthButton />
           </header>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
-import { Inter } from '@next/font/google'
+'use client'
 
-const inter = Inter({ subsets: ['latin'] })
+import { useSession } from 'next-auth/react'
 
 export default function Home() {
+  const { data: session } = useSession()
 
+  if (session) {
+    return (
+      <main>
+        <h1>Welcome {session.user?.name}</h1>
+      </main>
+    )
+  }
   return (
     <main>
-      
+      <h1>Welcome to Project-Rock</h1>
     </main>
   )
 }
