1652d929a127a1f0351ffc278b39ce1675aa3448
Corey Sutcliffe <Corey.j.sutcliffe@gmail.com>
2023-03-08 12:08:08 +0000

    added env to gitingnore

--- src/server/routers/userRouter.ts
+++ src/server/routers/userRouter.ts
@@ -1,8 +1,15 @@
-import { z } from 'zod';
-import { router, procedure } from '@/lib/trpc';
+import { z } from "zod";
+import { router, procedure } from "@/lib/trpc";
+import prisma from "@/lib/prisma";
 
 export const userRouter = router({
-    userList: procedure.query(() => {
-        return [{ id: 1, name: 'John' }, { id: 2, name: 'Jane' }];
-    })
-})
+  getUser: procedure.input(z.string()).query(async (req) => {
+    const { input } = req;
+    const result = await prisma.user.findUnique({
+      where: {
+        email: input,
+      },
+    });
+    return result;
+  }),
+});
