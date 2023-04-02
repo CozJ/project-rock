ed79e504020810dd05f34b225234014ab084567b
Corey Sutcliffe <Corey.j.sutcliffe@gmail.com>
2023-04-02 19:57:20 +0100

    Create migration.sql

---
+++ prisma/migrations/20230402185705_test/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `section` on the `ClimbingRoutesAttempts` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "ClimbingRoutesAttempts" DROP COLUMN "section";
