cb50928b605ecc25714363e84856c5791fb631e4
Corey Sutcliffe <Corey.j.sutcliffe@gmail.com>
2023-04-02 19:29:33 +0100

    Update climbingRoutesAttemptsRouter.ts

--- src/server/api/routers/climbingRoutesAttemptsRouter.ts
+++ src/server/api/routers/climbingRoutesAttemptsRouter.ts
@@ -7,5 +7,19 @@ import {
 } from "@/server/api/trpc";
 
 export const climbingRoutesAttemptsRouter = createTRPCRouter({
-  
+  createAttempt: protectedProcedure
+  .input(
+    z.object({
+      routeId: z.string(),
+      type: z.string(),
+    })
+  )
+  .mutation(({ ctx, input }) => {
+    return ctx.prisma.climbingRoutesAttempts.create({
+      data: {
+        routeId: input.routeId,
+        type: input.type,
+      },
+    });
+  }),
 });
