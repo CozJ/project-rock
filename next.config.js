336dc2cc9f730706c0fa5caec34ae38d5e764b2a
Corey Sutcliffe <Corey.j.sutcliffe@gmail.com>
2023-04-09 15:46:15 +0100

    file tray key and image auth

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   experimental: {
     appDir: false,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.amazonaws.com',
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
