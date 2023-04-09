94bf94206fb0b313b1eec3a37d28e7e9dd053f97
Corey Sutcliffe <Corey.j.sutcliffe@gmail.com>
2023-04-09 20:58:13 +0100

    prompt login page

--- src/components/auth/promptLogin.tsx
+++ src/components/auth/promptLogin.tsx
@@ -1,13 +1,29 @@
-import React from 'react'
-import { AuthButton } from './AuthButton'
+import React from "react";
+import { AuthButton } from "./AuthButton";
 
 export const PromptLogin = () => {
   return (
     <>
-      <div className="flex h-72 w-full flex-row items-center justify-center text-2xl">
-        <h1>Welcome to Project-Rock, Please&nbsp;</h1>
-        <AuthButton className="hover:underline" />
+      <div className="flex h-full w-full flex-row items-center justify-center text-2xl">
+        <div className="m-4 flex h-96 w-full max-w-3xl flex-col rounded-lg border shadow-lg md:flex-row">
+          <div className="h-1/4 w-full max-md:rounded-t-lg bg-gradient-to-b from-slate-800 via-slate-700 to-slate-600 md:h-full md:w-1/4 md:rounded-l-lg"></div>
+          <div className="h-3/4 w-full p-4 md:h-full md:w-3/4">
+            <div className="flex h-full flex-col justify-between">
+              <div>
+                <h1 className="text-4xl text-slate-800">
+                  Welcome to Project-Rock
+                </h1>
+                <p className="text-slate-800">
+                  Please login to continue to the site.
+                </p>
+              </div>
+              <div className="flex justify-end">
+                <AuthButton className="hover:underline text-slate-200 bg-slate-600 p-1 px-2 rounded-lg hover:bg-slate-800" />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -13,11 +13,15 @@ export default function RootLayout({
     <>
       <header className="flex w-full flex-row items-center justify-between p-2 shadow-xl">
         <Link href={"/"}>
-          <h1 className="text-2xl font-semibold text-slate-600">Project-Rock</h1>
+          <h1 className="text-2xl font-semibold text-slate-600">
+            Project-Rock
+          </h1>
         </Link>
-        <AuthButton className="rounded-lg bg-slate-600 text-slate-100 p-2 px-4 font-semibold" />
+        <AuthButton className="rounded-lg bg-slate-600 p-2 px-4 font-semibold text-slate-100" />
       </header>
-      <main className={inter.className}>{children}</main>
+      <main className={`${inter.className} min-h-full min-w-full`}>
+        {children}
+      </main>
     </>
   );
 }
