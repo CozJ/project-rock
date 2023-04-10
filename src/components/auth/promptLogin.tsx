d073634dbf4d58fbe368f92554e9d3c8d3ef35a9
Corey Sutcliffe <Corey.j.sutcliffe@gmail.com>
2023-04-10 17:52:35 +0100

    img for login prompt

--- src/components/auth/promptLogin.tsx
+++ src/components/auth/promptLogin.tsx
@@ -1,23 +1,24 @@
-import React from "react";
 import { SignInButton } from "./SignInButton";
+import Image from "next/image";
 
 export const PromptLogin = () => {
   return (
     <>
-      <div className="flex h-full w-full flex-row items-center justify-center text-2xl">
-        <div className="m-4 flex h-96 w-full max-w-4xl flex-col rounded-lg border shadow-lg md:flex-row">
-          <div className="h-1/4 w-full max-md:rounded-t-lg bg-gradient-to-b from-slate-800 via-slate-700 to-slate-600 md:h-full md:w-1/4 md:rounded-l-lg"></div>
-          <div className="h-3/4 w-full p-4 md:h-full md:w-3/4">
-            <div className="flex h-full flex-col justify-between">
-              <div>
-                <h1 className="text-4xl text-slate-800">
-                  Welcome to Project-Rock
-                </h1>
-                <p className="text-slate-800">
-                  Please login to continue to the site.
-                </p>
-              </div>
-              <div className="flex justify-end">
+      <div className="flex w-full flex-col md:flex-row">
+        <div className="h-1/3 max-w-full bg-slate-400 bg-[url('/login-page-hero.jpg')] bg-cover md:h-full md:w-1/3"></div>
+        <div className="h-2/3 w-full md:h-full md:w-2/3 ">
+          <div className="flex h-full flex-col items-center justify-center">
+            <div className="text-slate-800">
+              <h1 className="flex flex-row border-b pb-2 text-6xl">
+                PROJECT-ROCK
+              </h1>
+              <h2 className="mt-8 text-2xl">
+                The best way to track your climbing progress
+              </h2>
+              <p className="p-1 text-lg">Track your climbing progress</p>
+              <p className="p-1 text-lg">Store information about your routes</p>
+              <p className="p-1 text-lg">Analyse your progress</p>
+              <div className="w-full flex justify-end">
                 <SignInButton />
               </div>
             </div>
