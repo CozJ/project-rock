a2ce7408bd071bc467b3d5056c5638ac7cb787d6
Corey Sutcliffe <Corey.j.sutcliffe@gmail.com>
2023-04-11 15:53:15 +0100

    inline button style change

--- src/components/common/InlineUpdateStyle.tsx
+++ src/components/common/InlineUpdateStyle.tsx
@@ -98,17 +98,17 @@ export const InlineUpdateStyle = (props: InlineStyleEditProps) => {
               </label>
             </div>
             {errors.style && <p className="text-red-500">Style is required</p>}
-            <div className="my-1 w-36 font-semibold text-slate-100">
+            <div className="mx-2 flex w-auto flex-row py-1 font-semibold text-slate-100">
               <button
                 type="button"
                 onClick={handleCancel}
-                className=" max-h-fit w-1/2 rounded-l-lg  bg-slate-300 p-2 px-2 font-semibold hover:bg-slate-800"
+                className=" max-h-fit w-16 rounded-l-lg  bg-slate-300 p-1 px-1 hover:bg-slate-800"
               >
                 Cancel
               </button>
               <button
                 type="submit"
-                className="max-h-fit w-1/2 rounded-r-lg  bg-slate-600 p-2 px-2 hover:bg-slate-800"
+                className="max-h-fit w-16 rounded-r-lg  bg-slate-600 p-1 px-1 hover:bg-slate-800"
               >
                 Save
               </button>
